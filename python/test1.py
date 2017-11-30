06919d6eac1aac668abcbea08336655dc8f2ceca
Johannes Nix <johannes.nix@stfc.ac.uk>
2017-11-30 17:14:34 +0000

    Add test for getPositions command.

--- python/test1.py
+++ python/test1.py
@@ -1,6 +1,6 @@
 import fpu_driver
 
-NUM_FPUS = 5
+NUM_FPUS = 100
 ga = fpu_driver.GatewayAddress("127.0.0.1", 4700)
 
 gd = fpu_driver.GridDriver(NUM_FPUS)
@@ -9,6 +9,18 @@ print("initializing driver: ", gd.initializeDriver())
 
 
 print("connecting grid:", gd.connect([ga]))
+
+
+print("getting grid state:")
+gs = gd.getGridState()
+
+print("positions before:", [ (gs.FPU[i].alpha_steps, gs.FPU[i].alpha_steps) for i in range(NUM_FPUS)])
+
+print("getting positions:")
+gd.getPositions(gs)
+
+print("positions:", [ (gs.FPU[i].alpha_steps, gs.FPU[i].alpha_steps) for i in range(NUM_FPUS)])
+
 
 
 
