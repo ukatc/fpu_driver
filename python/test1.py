d1cd2e2eefef65ba74c40a24d356f5a170b5df8a
Johannes Nix <johannes.nix@stfc.ac.uk>
2017-12-01 18:09:02 +0000

    Use a smaller number of FPUs.

--- python/test1.py
+++ python/test1.py
@@ -1,6 +1,6 @@
 import fpu_driver
 
-NUM_FPUS = 100
+NUM_FPUS = 50
 ga = fpu_driver.GatewayAddress("127.0.0.1", 4700)
 
 gd = fpu_driver.GridDriver(NUM_FPUS)
@@ -14,12 +14,12 @@ print("connecting grid:", gd.connect([ga]))
 print("getting grid state:")
 gs = gd.getGridState()
 
-print("positions before:", [ (gs.FPU[i].alpha_steps, gs.FPU[i].alpha_steps) for i in range(NUM_FPUS)])
+print("positions before:", [ (gs.FPU[i].alpha_steps, gs.FPU[i].beta_steps) for i in range(NUM_FPUS)])
 
 print("getting positions:")
 gd.getPositions(gs)
 
-print("positions:", [ (gs.FPU[i].alpha_steps, gs.FPU[i].alpha_steps) for i in range(NUM_FPUS)])
+print("positions:", [ (gs.FPU[i].alpha_steps, gs.FPU[i].beta_steps) for i in range(NUM_FPUS)])
 
 
 
