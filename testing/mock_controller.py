947888735b43dd2bdf34bf1439b2779c5111a71f
Johannes Nix <johannes.nix@stfc.ac.uk>
2017-12-01 12:04:04 +0000

    Split mock controller into own module.

---
+++ testing/mock_controller.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+"""
+This is a mock FPU grid controller which decodes and
+synthesizes a response.
+
+"""
+
+from __future__ import print_function
+import codec
+
+
+def command_handler(cmd, socket):
+    print("command decoded bytes are:", cmd)
+    gateway_id = gateway_map[socket.getsockname()]
+    busid = cmd[0]
+    canid = cmd[1] + (cmd[2] << 8)
+    fpuid = canid & 0x7f
+    priority = (canid >> 7)
+    command_id = cmd[3]
+    print("CAN command to gw %i, bus %i, fpu # %i (priority %i), command id=%i"
+          % (gateway_id, busid, fpuid, priority, command_id))
+
+    tx_busid = busid
+    tx_prio = 0x02
+    tx_canid = (tx_prio << 7) | fpuid
+    tx0_fpuid = fpuid
+    tx1_cmdid = command_id
+    tx2_status = 0
+    tx3_errcode = 0
+    pos = 2000
+    tx4_count0 = pos & 0xff
+    tx5_count1 = (pos >> 8) & 0xff
+    tx6_count2 = (pos >> 16) & 0xff
+    
+    
+    resp = [ tx_busid,
+             (tx_canid & 0xff),
+             ((tx_canid >> 8) & 0xff),
+             tx0_fpuid,
+             tx1_cmdid,
+             tx2_status,
+             tx3_errcode,
+             tx4_count0,
+             tx5_count1,
+             tx6_count2]
+    
+    response = codec.encode(resp)
+    socket.sendall(response)
+    #print("echoed %r" % response)
--- testing/mock_gateway.py
+++ testing/mock_gateway.py
@@ -16,44 +16,9 @@ import array
 
 import codec
 
-def command_handler(cmd, socket):
-    print("command decoded bytes are:", cmd)
-    gateway_id = gateway_map[socket.getsockname()]
-    busid = cmd[0]
-    canid = cmd[1] + (cmd[2] << 8)
-    fpuid = canid & 0x7f
-    priority = (canid >> 7)
-    command_id = cmd[3]
-    print("CAN command to gw %i, bus %i, fpu # %i (priority %i), command id=%i"
-          % (gateway_id, busid, fpuid, priority, command_id))
+import mock_controller
+from mock_controller import command_handler
 
-    tx_busid = busid
-    tx_prio = 0x02
-    tx_canid = (tx_prio << 7) | fpuid
-    tx0_fpuid = fpuid
-    tx1_cmdid = command_id
-    tx2_status = 0
-    tx3_errcode = 0
-    pos = 2000
-    tx4_count0 = pos & 0xff
-    tx5_count1 = (pos >> 8) & 0xff
-    tx6_count2 = (pos >> 16) & 0xff
-    
-    
-    resp = [ tx_busid,
-             (tx_canid & 0xff),
-             ((tx_canid >> 8) & 0xff),
-             tx0_fpuid,
-             tx1_cmdid,
-             tx2_status,
-             tx3_errcode,
-             tx4_count0,
-             tx5_count1,
-             tx6_count2]
-    
-    response = codec.encode(resp)
-    socket.sendall(response)
-    #print("echoed %r" % response)
 
 # this handler will be run for each incoming connection in a dedicated greenlet
 def echo(socket, address):
@@ -73,8 +38,8 @@ def echo(socket, address):
 if __name__ == '__main__':
     ports = [ 4700, 4701, 4702]
     ip = '127.0.0.1'
-    gateway_map = { (ip, ports[i]) : i for i in range(len(ports))  }
-    print("gateway map:", gateway_map)
+    mock_controller.gateway_map = { (ip, ports[i]) : i for i in range(len(ports))  }
+    print("gateway map:", mock_controller.gateway_map)
     servers = [ StreamServer((ip, p), echo, 50) for p in ports]
     # to start the servers asynchronously, we use its start() method;
     # we use blocking serve_forever() for the third and last connection.
