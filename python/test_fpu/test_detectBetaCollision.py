22072b75c3500b0398c5324b83e2e278d5c1f738
Johannes Nix <johannes.nix@stfc.ac.uk>
2018-02-13 13:11:47 +0000

    Add Alpha limit detection.

---
+++ python/test_fpu/test_detectBetaCollision.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+import time
+
+import FpuGridDriver
+
+from FpuGridDriver import TEST_GATEWAY_ADRESS_LIST
+
+from fpu_commands import *
+
+NUM_FPUS = 1
+
+
+gd = FpuGridDriver.GridDriver(NUM_FPUS)
+
+print("initializing driver: ", gd.initializeDriver())
+
+
+print("connecting grid:", gd.connect(address_list=TEST_GATEWAY_ADRESS_LIST))
+
+
+print("getting grid state:")
+gs = gd.getGridState()
+
+
+
+print("issuing findDatum:")
+gd.findDatum(gs)
+print("findDatum finished")
+
+
+
+steps_up = [ (0, 100) for i in range(125) ]
+
+steps_down = [ (0, -100) for i in range(125) ]
+
+wt_up = { 0: steps_up, }
+
+wt_down = { 0: steps_down, }
+
+
+print("issuing configMotion:")
+
+rv = gd.configMotion(wt_up, gs)
+
+print("configMotion finished with return value", rv)
+
+
+print("issuing 1st executeMotion")
+gd.executeMotion(gs)
+print("positions:", list_positions(gs))
+
+gd.repeatMotion(gs)
+print("issuing 2nd executeMotion")
+gd.executeMotion(gs)
+print("positions:", list_positions(gs))
+
+gd.repeatMotion(gs)
+print("issuing 3rd executeMotion")
+gd.executeMotion(gs)
+print("positions:", list_positions(gs))
+
+
+#gd.reverseMotion(gs)
+#print("issuing 4th executeMotion")
+#gd.executeMotion(gs)
+#print("positions:", list_positions(gs))
+#
+#gd.reverseMotion(gs)
+#print("issuing 5th executeMotion")
+#gd.executeMotion(gs)
+#print("positions:", list_positions(gs))
+#
+#gd.reverseMotion(gs)
+#print("issuing 6th executeMotion")
+#gd.executeMotion(gs)
+#print("positions (should be back to origin):", list_positions(gs))
+#
+
+
+
+
+
+                       
+
+
+
+
+
+
