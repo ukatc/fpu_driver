201eae85f9b27d76ea39be78b12a72ac6095b5c3
Johannes Nix <johannes.nix@stfc.ac.uk>
2018-10-29 13:45:49 +0000

    Check length of received response

--- src/handleFPUResponse.C
+++ src/handleFPUResponse.C
@@ -68,12 +68,6 @@ void handleFPUResponse(const EtherCANInterfaceConfig& config,
                        const int blen, TimeOutList& timeout_list,
                        int &count_pending)
 {
-    E_CAN_COMMAND cmd_id = static_cast<E_CAN_COMMAND>(data[1] & COMMAND_CODE_MASK);
-    uint8_t sequence_number = data[0];
-
-    assert(blen <= 8);
-    assert(blen >= 4);
-
     void (*handler) (const EtherCANInterfaceConfig&,
 		     const int,
 		     t_fpu_state&,
@@ -83,50 +77,32 @@ void handleFPUResponse(const EtherCANInterfaceConfig& config,
 		     TimeOutList&t,
 		     const E_CAN_COMMAND,
 		     const uint8_t) = nullptr;
-
-
+    
+    E_CAN_COMMAND cmd_id = static_cast<E_CAN_COMMAND>(data[1] & COMMAND_CODE_MASK);
+    int required_length = 8;
 
     switch (cmd_id)
     {
-    case CCMD_CONFIG_MOTION   : handler = &handle_ConfigMotion_response; break;
-
-    case CCMD_EXECUTE_MOTION  : handler = &handle_ExecuteMotion_response; break;
-
-    case CMSG_FINISHED_MOTION: handler = &handle_FinishedMotion_message; break;
-
-    case CCMD_ABORT_MOTION    : handler = &handle_AbortMotion_response; break;
-
-    case CMSG_WARN_CANOVERFLOW    : handler = &handle_WarnCANOverflow_warning; break;
-
-    case CCMD_GET_COUNTER_DEVIATION  : handler = &handle_GetCounterDeviation_response; break;
-
-    case CCMD_PING_FPU        : handler = &handle_PingFPU_response; break;
-
-    case CCMD_REPEAT_MOTION        : handler = &handle_RepeatMotion_response; break;
-
-    case CCMD_REVERSE_MOTION        : handler = &handle_ReverseMotion_response; break;
-
-    case CCMD_RESET_FPU       : handler = &handle_ResetFPU_response; break;
-
-    case CCMD_FIND_DATUM : handler = handle_FindDatum_response; break;
-
-    case CMSG_FINISHED_DATUM : handler = &handle_FinishedDatum_message; break;
-
-    case CMSG_WARN_COLLISION_BETA: handler = &handle_WarnCollisionBeta_warning; break;
-	
-    case CMSG_WARN_LIMIT_ALPHA: handler = &handle_WarnLimitAlpha_warning; break;
-
-    case CCMD_ENABLE_BETA_COLLISION_PROTECTION: handler = &handle_EnableBetaCollisionProtection_response; break;
-
-    case CCMD_FREE_BETA_COLLISION: handler = &handle_FreeBetaCollision_response; break;
-
-    case CCMD_SET_USTEP_LEVEL: handler = &handle_SetUStepLevel_response; break;
-
-    case CCMD_READ_REGISTER : handler = &handle_ReadRegister_response; break;
-
-    case CCMD_WRITE_SERIAL_NUMBER: handler = &handle_WriteSerialNumber_response; break;
-
-    case CCMD_READ_SERIAL_NUMBER  : handler = & handle_ReadSerialNumber_response; break;
+    case CCMD_ABORT_MOTION                     : handler = &handle_AbortMotion_response; break;
+    case CCMD_CONFIG_MOTION                    : handler = &handle_ConfigMotion_response; required_length = 5; break;
+    case CCMD_ENABLE_BETA_COLLISION_PROTECTION : handler = &handle_EnableBetaCollisionProtection_response; break;
+    case CCMD_EXECUTE_MOTION                   : handler = &handle_ExecuteMotion_response; break;
+    case CCMD_FIND_DATUM                       : handler = handle_FindDatum_response; break;
+    case CCMD_FREE_BETA_COLLISION              : handler = &handle_FreeBetaCollision_response; break;
+    case CCMD_GET_COUNTER_DEVIATION            : handler = &handle_GetCounterDeviation_response; break;
+    case CCMD_PING_FPU                         : handler = &handle_PingFPU_response; break;
+    case CCMD_READ_REGISTER                    : handler = &handle_ReadRegister_response; required_length = 7; break;
+    case CCMD_READ_SERIAL_NUMBER               : handler = &handle_ReadSerialNumber_response; break;
+    case CCMD_REPEAT_MOTION                    : handler = &handle_RepeatMotion_response; break;
+    case CCMD_RESET_FPU                        : handler = &handle_ResetFPU_response; break;
+    case CCMD_REVERSE_MOTION                   : handler = &handle_ReverseMotion_response; break;
+    case CCMD_SET_USTEP_LEVEL                  : handler = &handle_SetUStepLevel_response; break;
+    case CCMD_WRITE_SERIAL_NUMBER              : handler = &handle_WriteSerialNumber_response; break;
+    case CMSG_FINISHED_DATUM                   : handler = &handle_FinishedDatum_message; break;
+    case CMSG_FINISHED_MOTION                  : handler = &handle_FinishedMotion_message; break;
+    case CMSG_WARN_CANOVERFLOW                 : handler = &handle_WarnCANOverflow_warning; break;
+    case CMSG_WARN_COLLISION_BETA              : handler = &handle_WarnCollisionBeta_warning; break;
+    case CMSG_WARN_LIMIT_ALPHA                 : handler = &handle_WarnLimitAlpha_warning; break;
 
     case CCMD_NO_COMMAND      :
     default:
@@ -137,18 +113,33 @@ void handleFPUResponse(const EtherCANInterfaceConfig& config,
                get_realtime(),
                cmd_id,
                fpu_id);
-        break;
+        return;
 
     }
+    if (blen != required_length)
+    {
+	LOG_RX(LOG_ERROR, "%18.6f : RX : "
+               "invalid message length (%i, should be %i) for command id # %i) received for FPU %i\n",
+               get_realtime(),
+	       blen,
+	       required_length,
+               cmd_id,
+               fpu_id);
+	return;
+    }
+
     if (handler != nullptr)
     {
+	uint8_t sequence_number = data[0];
+	
 	(*handler)(config, fpu_id, fpu, count_pending,  data, blen, timeout_list,
 		   cmd_id, sequence_number);
+	
+	timespec cur_time;
+	get_monotonic_time(cur_time);
+	fpu.last_updated = cur_time;
     }
 
-    timespec cur_time;
-    get_monotonic_time(cur_time);
-    fpu.last_updated = cur_time;
 
 }
 
