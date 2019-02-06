3da80ff005c2f8aee53399d737388c037d64f356
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-02-06 13:38:47 +0000

    Update manual to changed field names in protocol specification.

---
+++ python/example-short-script.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import FpuGridDriver
+from FpuGridDriver import MOCK_GATEWAY_ADRESS_LIST as gw_address
+from fpu_commands import list_positions, \
+       list_deviations, list_states, gen_wf
+# print the driver version, just to check
+print("The FPU driver version is:", FpuGridDriver.__version__,
+      ", CAN PROTOCOL version:", FpuGridDriver.CAN_PROTOCOL_VERSION)
+
+NUM_FPUS = 1 # set number of FPUs to 1
+gd = FpuGridDriver.GridDriver(NUM_FPUS)
+# connect to gateway with default IP 192.168.0.10
+print("connecting grid:", gd.connect(address_list=gw_address))
+
+print("getting grid state:")
+grid_state = gd.getGridState()
+
+
+print("issuing findDatum:")
+gd.findDatum(grid_state)
+print("findDatum finished")
+
+alpha_move = 45
+beta_move = 15
+
+# Generate the required waveform for one FPU
+waveform = gen_wf(alpha_move, beta_move)
+
+# upload the waveform to the FPU
+gd.configMotion(waveform, grid_state)
