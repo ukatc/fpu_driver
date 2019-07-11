471afa439ccd6225ded42ddca349f109a22e4267
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-07-11 20:30:29 +0100

    add constants in radian, to make internal units uniform

--- python/fpu_constants.py
+++ python/fpu_constants.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from math import pi
+from numpy import rad2deg, deg2rad
 
 RADIAN_TO_DEGREE = 180.0 / pi # conversion factor
 
@@ -18,6 +19,9 @@ DegreePerRevolution = 360.0
 StepsPerDegreeAlpha = float(StepsPerRevolution * AlphaGearRatio) / DegreePerRevolution
 StepsPerDegreeBeta = float(StepsPerRevolution * BetaGearRatio) / DegreePerRevolution
 
+StepsPerRadianAlpha = StepsPerDegreeAlpha / deg2rad(1.0)
+StepsPerRadianBeta = StepsPerDegreeBeta / deg2rad(1.0)
+
 ALPHA_MIN_DEGREE = -180.2
 ALPHA_MAX_DEGREE = +159.0
 BETA_MIN_DEGREE = -179.3
@@ -31,6 +35,9 @@ ALPHA_MAX_HARDSTOP_DEGREE = +181.8
 
 ALPHA_DATUM_OFFSET=-180.0
 BETA_DATUM_OFFSET=0.0
+
+ALPHA_DATUM_OFFSET_RAD= deg2rad(ALPHA_DATUM_OFFSET)
+BETA_DATUM_OFFSET_RAD= deg2rad(BETA_DATUM_OFFSET)
 
 DEFAULT_FREE_BETA_RETRIES=3
 FREE_BETA_STEPCOUNT=10
@@ -51,4 +58,3 @@ ALPHA_OVERFLOW_COUNT = ALPHA_UNDERFLOW_COUNT + (1 << 16) -1
 
 BETA_UNDERFLOW_COUNT = - 0x8000
 BETA_OVERFLOW_COUNT = BETA_UNDERFLOW_COUNT + (1 << 16) -1
-
