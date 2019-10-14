9bac92e1e69183055333ff8c4e5b0c5d69014a7c
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-10-14 15:08:01 +0100

    allow for serial numbers with a length of up to six ASCII chars

--- python/flash-range.sh
+++ python/flash-range.sh
@@ -6,7 +6,7 @@
 set -e
 for i in $(seq 0 $(($1 - 1)) );
  do
-	 sn=$(printf "MP%03i" $i);
+	 sn=$(printf "MP%04i" $i);
 	 ./fpu-admin init $sn -180 0 --mockup;
 	 ./fpu-admin flash --reuse_sn $sn $i --mockup;
  done
