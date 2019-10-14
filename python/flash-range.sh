0fb3eba6ac0bc98ef3cdd9ed9ae90dfa716cc7a7
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-10-14 14:14:34 +0100

    return error status from fpu-admin script
    Also, stop flash-range test set-up script on error

--- python/flash-range.sh
+++ python/flash-range.sh
@@ -3,6 +3,7 @@
 # this initializes a number of simulated FPUs to standard serial
 # numbers, their positions all being set to the default position.
 
+set -e
 for i in $(seq 0 $(($1 - 1)) );
  do
 	 sn=$(printf "MP%03i" $i);
