f6a6af3fbd2fd51d11615326dd028071304f53e8
Johannes Nix <johannes.nix@stfc.ac.uk>
2019-10-14 13:47:18 +0100

    add comment to test configuration script

--- python/flash-range.sh
+++ python/flash-range.sh
@@ -1,4 +1,9 @@
- for i in $(seq 0 $(($1 - 1)) );
+#!/bin/bash
+
+# this initializes a number of simulated FPUs to standard serial
+# numbers, their positions all being set to the default position.
+
+for i in $(seq 0 $(($1 - 1)) );
  do
 	 sn=$(printf "MP%03i" $i);
 	 ./fpu-admin init $sn -180 0 --mockup;
