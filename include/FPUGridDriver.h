62d93987d412b744790c07c3425fbcbf75f0337f
bwillemse <bwemail>
2020-04-28 13:05:01 +0100

    Added new FPUGridDriver.C/h skeleton files

---
+++ include/FPUGridDriver.h
@@ -0,0 +1,47 @@
+// -*- mode: c++ -*-
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 UKRI. See file "LICENSE" for license information.
+//
+//
+// Who       When        What
+// --------  ----------  -------------------------------------------------------
+// bwillemse 2020-04-28  TODO: Put comment here
+//------------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+// NAME FPUGridDriver.h
+//
+// TODO: Put description here
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef FPUGRIDDRIVER_H
+#define FPUGRIDDRIVER_H
+
+namespace mpifps
+{
+
+class UnprotectedGridDriver
+{
+public:
+    UnprotectedGridDriver();
+
+private:
+
+};
+
+class GridDriver
+{
+public:
+    GridDriver();
+
+private:    
+
+};
+
+
+} // namespace mpifps
+
+#endif // FPUGRIDDRIVER_H
---
+++ src/FPUGridDriver.C
@@ -0,0 +1,53 @@
+// -*- mode: c++ -*-
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2020 UKRI. See file "LICENSE" for license information.
+//
+//
+// Who       When        What
+// --------  ----------  -------------------------------------------------------
+// bwillemse 2020-04-28  TODO: Put comment here
+//------------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+// NAME FPUGridDriver.C
+//
+// TODO: Put description here
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+// ********** NOTE: This file is Bart's work in progress for converting the
+// classes and functions in FPUGridDriver.py from Python to C++.
+
+
+#include "FPUGridDriver.h"
+
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
+namespace mpifps
+{
+
+//==============================================================================
+
+UnprotectedGridDriver::UnprotectedGridDriver()
+{
+    
+}
+
+//==============================================================================
+
+GridDriver::GridDriver()
+{
+    
+}
+
+//==============================================================================
+
+
+} // namespace mpifps
+
