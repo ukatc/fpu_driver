49e317d577807db4aba8fa7ccc2ddda45823fb7e
bwillemse <bwemail>
2020-06-04 15:49:14 +0100

    ProtectionDB WIP: Moved all unit test functions to bottom of ProtectionDB.C

--- src/ProtectionDB.C
+++ src/ProtectionDB.C
@@ -215,7 +215,211 @@ ProtectionDB::~ProtectionDB()
 }
 
 // -----------------------------------------------------------------------------
-// Unit test functions
+#else // NOT PROTECTIONDB_RAII_VERSION
+// -----------------------------------------------------------------------------
+
+int ProtectionDB::doStuff()
+{
+    int major = 1;
+    int minor = 2;
+    int patch = 3;
+    char *lmdb_version_str = mdb_version(&major, &minor, &patch);
+
+    return 456;
+}
+
+bool ProtectionDB::getRawField(MDB_txn &txn, MDB_dbi dbi,
+                               const char serial_number[],
+                               const char subkey[], MDB_val &data_val_ret)
+{
+    std::string key_str = std::string(serial_number) + keystr_separator_char +
+                          subkey;
+    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
+
+    int mdb_result = mdb_get(&txn, dbi, &key_val, &data_val_ret);
+    
+    if (mdb_result == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void ProtectionDB::putField(MDB_txn &txn, MDB_dbi dbi, const char serial_number[],
+                            const char subkey[], MDB_val &data_val)
+{
+    // Create ASCII key of form <serial_number><separator><subkey>
+    // IMPORTANT: serial_number and subkey must not contain the
+    // keystr_separator_char character
+    std::string key_str = std::string(serial_number) + keystr_separator_char +
+                          subkey;
+    
+    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
+
+    // TODO: Check if final flags argument below is OK
+    int mdb_result = mdb_put(&txn, dbi, &key_val, &data_val, 0x0);
+
+    // TODO: Call mdb_txn_commit() here?? Check against the Python version
+    // AND where to call mdb_env_sync() to flush to disk?
+    
+    // TODO: Return a result value
+}
+
+void ProtectionDB::put_counters(MDB_txn &txn, MDB_dbi dbi, 
+                                const char serial_number[],
+                                const FpuCounters &fpu_counters)
+{
+    MDB_val data_val;
+    
+    data_val.mv_data = fpu_counters.getRawData(data_val.mv_size); 
+
+    putField(txn, dbi, serial_number, counters_keystr, data_val);
+
+    // TODO: Return a result value
+}
+
+
+// -----------------------------------------------------------------------------
+#endif // NOT PROTECTIONDB_RAII_VERSION
+// -----------------------------------------------------------------------------
+
+MDB_env *open_database_env(bool mockup)
+{
+    // TODO: Must only call exactly once for a particular LMDB file in this
+    // process (see the LMDB documentation) - enforce this somehow?
+    MDB_env *env_ptr = nullptr;
+    
+    std::string dir_str = protectiondb_get_dir_from_linux_env(mockup);
+    if (!dir_str.empty())
+    {
+        env_ptr = protectiondb_open_env(dir_str);
+    }
+        
+    return env_ptr;
+}            
+
+// -----------------------------------------------------------------------------
+
+std::string protectiondb_get_dir_from_linux_env(bool mockup)
+{
+    // Provides Linux directory for the protection database based upon the
+    // Linux environment variables FPU_DATABASE_MOCKUP and FPU_DATABASE, and
+    // value of mockup. If unsuccessful then the returned string is empty.
+    // The environment variables need to be of the form e.g. "/var/lib/fpudb",
+    // and must **NOT** have a final "/" character
+
+    char *dir_c_str = nullptr;
+    std::string dir_str_ret;
+    
+    if (mockup)
+    {
+        dir_c_str = getenv("FPU_DATABASE_MOCKUP");
+        if (dir_c_str == nullptr)
+        {
+            dir_c_str = getenv("FPU_DATABASE");
+            if (dir_c_str != nullptr)
+            {
+                dir_str_ret = dir_c_str;
+                dir_str_ret += "_mockup";
+            }
+        }
+        else
+        {
+            dir_str_ret = dir_c_str;
+        }
+    }
+    else
+    {
+        dir_c_str = getenv("FPU_DATABASE");
+        if (dir_c_str != nullptr)
+        {
+            dir_str_ret = dir_c_str;
+        }
+    }
+    
+    return dir_str_ret;
+}
+
+// -----------------------------------------------------------------------------
+    
+MDB_env *protectiondb_open_env(const std::string &dir_str)
+{
+    // TODO: Must only call exactly once for a particular LMDB file in this
+    // process (see the LMDB documentation) - enforce this somehow?
+    
+    MDB_env *env_ptr = nullptr;
+    size_t dbsize;
+
+    // Needs 64 bit (large file support) for normal database size
+    // TODO: In original Python code, 32/64-bit OS check was done - but is
+    // this possible from C++? Or can we assume it's always 64-bit?
+    // (can make it a requirement?)
+    // if (platform.architecture()[0] == "64bit")  // Original Python
+    if (1)
+    {
+        dbsize = 5 * 1024L * 1024L * 1024L;
+    }
+    else
+    {
+        dbsize = 5 * 1024L * 1024L;
+    }
+
+    //.......................
+    // TODO: Python to c++ conversion WIP
+    // See https://lmdb.readthedocs.io/en/release/ -> Environment class
+    // env = lmdb.open(database_file_name, max_dbs=10, map_size=dbsize)
+
+    // TODO: In the original Python code, the following are defaulted - see
+    // https://lmdb.readthedocs.io/en/release/ -> Environment class - 
+    // max_spare_txns=1
+    // create=True      <== **IMPORTANT** - create directory if doesn't exist?
+
+    int mdb_result = mdb_env_create(&env_ptr);
+    if (mdb_result == 0)
+    {
+        mdb_result = mdb_env_set_maxdbs(env_ptr, 10);
+        if (mdb_result == 0)
+        {
+            mdb_result = mdb_env_set_mapsize(env_ptr, dbsize);
+        }
+
+        if (mdb_result == 0)
+        {
+            // TODO: The following keeps compatibility with the original
+            // Python code defaults, but are this many readers needed?
+            mdb_result = mdb_env_set_maxreaders(env_ptr, 126);
+        }
+
+        if (mdb_result == 0)
+        {
+            // N.B. Using default flags for now, so flags value is 0x0
+            // TODO: Check the required state of MDB_NOTLS flag
+            unsigned int flags = 0x0;
+            mdb_result = mdb_env_open(env_ptr, dir_str.c_str(),
+                                      flags, 0755);
+        }
+
+        if (mdb_result != 0)
+        {
+            // Failure - call mdb_env_close() to discard the MDB_env handle
+            mdb_env_close(env_ptr);
+            env_ptr = nullptr;
+        }
+    }
+
+    //.......................
+
+    // TODO: Return mdb_result somehow as well?
+
+    return env_ptr;
+}
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Unit test functions follow
+// *****************************************************************************
+// *****************************************************************************
 
 static std::string createRandomFpuTestSerialNumber()
 {
@@ -235,6 +439,9 @@ static bool protectiondb_test_fpu_counter_writing(ProtectionDB &protectiondb)
         FpuCounters fpu_counters;
         result_ok = fpudb_txn->putCounters("BWTest0001", fpu_counters);
     }
+    
+    // TODO HERE: Also read back the counters and compare against what was
+    // written (N.B. Need to populate the counters with some random values)
     
     return result_ok;
 }
@@ -400,6 +607,9 @@ static bool protectiondb_test_close_reopen(const std::string &dir_str)
         }
     }
     
+    // NOTE: If single-stepping through this function, can now do a dump of the
+    // database to check its contents, using e.g.: mdb_dump . -p -s fpu
+
     if (result_ok)
     {
         ProtectionDB protectiondb;
@@ -412,6 +622,9 @@ static bool protectiondb_test_close_reopen(const std::string &dir_str)
             result_ok = false;
         }
     }
+
+    // NOTE: If single-stepping through this function, can now do a dump of the
+    // database to check its contents, using e.g.: mdb_dump . -p -s fpu
 
     if (result_ok)
     {
@@ -448,75 +661,8 @@ bool protectiondb_test()
 }
 
 
-
-
-
-
-
 // -----------------------------------------------------------------------------
-#else // NOT PROTECTIONDB_RAII_VERSION
-// -----------------------------------------------------------------------------
-
-int ProtectionDB::doStuff()
-{
-    int major = 1;
-    int minor = 2;
-    int patch = 3;
-    char *lmdb_version_str = mdb_version(&major, &minor, &patch);
-
-    return 456;
-}
-
-bool ProtectionDB::getRawField(MDB_txn &txn, MDB_dbi dbi,
-                               const char serial_number[],
-                               const char subkey[], MDB_val &data_val_ret)
-{
-    std::string key_str = std::string(serial_number) + keystr_separator_char +
-                          subkey;
-    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
-
-    int mdb_result = mdb_get(&txn, dbi, &key_val, &data_val_ret);
-    
-    if (mdb_result == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-void ProtectionDB::putField(MDB_txn &txn, MDB_dbi dbi, const char serial_number[],
-                            const char subkey[], MDB_val &data_val)
-{
-    // Create ASCII key of form <serial_number><separator><subkey>
-    // IMPORTANT: serial_number and subkey must not contain the
-    // keystr_separator_char character
-    std::string key_str = std::string(serial_number) + keystr_separator_char +
-                          subkey;
-    
-    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
-
-    // TODO: Check if final flags argument below is OK
-    int mdb_result = mdb_put(&txn, dbi, &key_val, &data_val, 0x0);
-
-    // TODO: Call mdb_txn_commit() here?? Check against the Python version
-    // AND where to call mdb_env_sync() to flush to disk?
-    
-    // TODO: Return a result value
-}
-
-void ProtectionDB::put_counters(MDB_txn &txn, MDB_dbi dbi, 
-                                const char serial_number[],
-                                const FpuCounters &fpu_counters)
-{
-    MDB_val data_val;
-    
-    data_val.mv_data = fpu_counters.getRawData(data_val.mv_size); 
-
-    putField(txn, dbi, serial_number, counters_keystr, data_val);
-
-    // TODO: Return a result value
-}
-
+#ifndef PROTECTIONDB_RAII_VERSION // NOT PROTECTIONDB_RAII_VERSION
 // -----------------------------------------------------------------------------
 
 void protectiondb_test()
@@ -582,143 +728,9 @@ void protectiondb_test()
     // mdb_dbi_close()?
     // mdb_env_close()?
     // Any others?
-
 }
 
 // -----------------------------------------------------------------------------
 #endif // NOT PROTECTIONDB_RAII_VERSION
-// -----------------------------------------------------------------------------
-
-MDB_env *open_database_env(bool mockup)
-{
-    // TODO: Must only call exactly once for a particular LMDB file in this
-    // process (see the LMDB documentation) - enforce this somehow?
-    MDB_env *env_ptr = nullptr;
-    
-    std::string dir_str = protectiondb_get_dir_from_linux_env(mockup);
-    if (!dir_str.empty())
-    {
-        env_ptr = protectiondb_open_env(dir_str);
-    }
-        
-    return env_ptr;
-}            
-
-// -----------------------------------------------------------------------------
-
-std::string protectiondb_get_dir_from_linux_env(bool mockup)
-{
-    // Provides Linux directory for the protection database based upon the
-    // Linux environment variables FPU_DATABASE_MOCKUP and FPU_DATABASE, and
-    // value of mockup. If unsuccessful then the returned string is empty.
-    // The environment variables need to be of the form e.g. "/var/lib/fpudb",
-    // and must **NOT** have a final "/" character
-
-    char *dir_c_str = nullptr;
-    std::string dir_str_ret;
-    
-    if (mockup)
-    {
-        dir_c_str = getenv("FPU_DATABASE_MOCKUP");
-        if (dir_c_str == nullptr)
-        {
-            dir_c_str = getenv("FPU_DATABASE");
-            if (dir_c_str != nullptr)
-            {
-                dir_str_ret = dir_c_str;
-                dir_str_ret += "_mockup";
-            }
-        }
-        else
-        {
-            dir_str_ret = dir_c_str;
-        }
-    }
-    else
-    {
-        dir_c_str = getenv("FPU_DATABASE");
-        if (dir_c_str != nullptr)
-        {
-            dir_str_ret = dir_c_str;
-        }
-    }
-    
-    return dir_str_ret;
-}
-
-// -----------------------------------------------------------------------------
-    
-MDB_env *protectiondb_open_env(const std::string &dir_str)
-{
-    // TODO: Must only call exactly once for a particular LMDB file in this
-    // process (see the LMDB documentation) - enforce this somehow?
-    
-    MDB_env *env_ptr = nullptr;
-    size_t dbsize;
-
-    // Needs 64 bit (large file support) for normal database size
-    // TODO: In original Python code, 32/64-bit OS check was done - but is
-    // this possible from C++? Or can we assume it's always 64-bit?
-    // (can make it a requirement?)
-    // if (platform.architecture()[0] == "64bit")  // Original Python
-    if (1)
-    {
-        dbsize = 5 * 1024L * 1024L * 1024L;
-    }
-    else
-    {
-        dbsize = 5 * 1024L * 1024L;
-    }
-
-    //.......................
-    // TODO: Python to c++ conversion WIP
-    // See https://lmdb.readthedocs.io/en/release/ -> Environment class
-    // env = lmdb.open(database_file_name, max_dbs=10, map_size=dbsize)
-
-    // TODO: In the original Python code, the following are defaulted - see
-    // https://lmdb.readthedocs.io/en/release/ -> Environment class - 
-    // max_spare_txns=1
-    // create=True      <== **IMPORTANT** - create directory if doesn't exist?
-
-    int mdb_result = mdb_env_create(&env_ptr);
-    if (mdb_result == 0)
-    {
-        mdb_result = mdb_env_set_maxdbs(env_ptr, 10);
-        if (mdb_result == 0)
-        {
-            mdb_result = mdb_env_set_mapsize(env_ptr, dbsize);
-        }
-
-        if (mdb_result == 0)
-        {
-            // TODO: The following keeps compatibility with the original
-            // Python code defaults, but are this many readers needed?
-            mdb_result = mdb_env_set_maxreaders(env_ptr, 126);
-        }
-
-        if (mdb_result == 0)
-        {
-            // N.B. Using default flags for now, so flags value is 0x0
-            // TODO: Check the required state of MDB_NOTLS flag
-            unsigned int flags = 0x0;
-            mdb_result = mdb_env_open(env_ptr, dir_str.c_str(),
-                                      flags, 0755);
-        }
-
-        if (mdb_result != 0)
-        {
-            // Failure - call mdb_env_close() to discard the MDB_env handle
-            mdb_env_close(env_ptr);
-            env_ptr = nullptr;
-        }
-    }
-
-    //.......................
-
-    // TODO: Return mdb_result somehow as well?
-
-    return env_ptr;
-}
-
 // -----------------------------------------------------------------------------
 
