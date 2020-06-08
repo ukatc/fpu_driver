f99db080d7983fe0652460b7d7f8fed79480af43
bwillemse <bwemail>
2020-06-08 12:10:28 +0100

    ProtectionDB: Integrated protectionDB_OpenEnv() into ProtectionDB::open(). moved old code to bottom

--- src/ProtectionDB.C
+++ src/ProtectionDB.C
@@ -54,10 +54,6 @@ static const char *keystr_separator_char = "#";
 
 static std::string protectionDB_GetDirFromLinuxEnv(bool mockup);
 
-// -----------------------------------------------------------------------------
-#ifdef PROTECTIONDB_RAII_VERSION
-// -----------------------------------------------------------------------------
-
 static MDB_val createFpuDbKeyVal(const char serial_number[],
                                  const char subkey[])
 {
@@ -170,9 +166,68 @@ ProtectionDB::FpuDbTxn::~FpuDbTxn()
 
 bool ProtectionDB::open(const std::string &dir_str)
 {
-    // TODO: Move protectionDB_OpenEnv() contents to here
+    // TODO: Must only call exactly once for a particular LMDB file in this
+    // process (see the LMDB documentation) - enforce this somehow?
+    
+    size_t dbsize;
 
-    mdb_env_ptr = protectionDB_OpenEnv(dir_str);
+    // Needs 64 bit (large file support) for normal database size
+    // TODO: In original Python code, 32/64-bit OS check was done - but is
+    // this possible from C++? Or can we assume it's always 64-bit?
+    // (can make it a requirement?)
+    // if (platform.architecture()[0] == "64bit")  // Original Python
+    if (1)
+    {
+        dbsize = 5 * 1024L * 1024L * 1024L;
+    }
+    else
+    {
+        dbsize = 5 * 1024L * 1024L;
+    }
+
+    //.......................
+    // TODO: Python to c++ conversion WIP
+    // See https://lmdb.readthedocs.io/en/release/ -> Environment class
+    // env = lmdb.open(database_file_name, max_dbs=10, map_size=dbsize)
+
+    // TODO: In the original Python code, the following are defaulted - see
+    // https://lmdb.readthedocs.io/en/release/ -> Environment class - 
+    // max_spare_txns=1
+    // create=True      <== **IMPORTANT** - create directory if doesn't exist?
+
+    int mdb_result = mdb_env_create(&mdb_env_ptr);
+    if (mdb_result == 0)
+    {
+        mdb_result = mdb_env_set_maxdbs(mdb_env_ptr, 10);
+        if (mdb_result == 0)
+        {
+            mdb_result = mdb_env_set_mapsize(mdb_env_ptr, dbsize);
+        }
+
+        if (mdb_result == 0)
+        {
+            // TODO: The following keeps compatibility with the original
+            // Python code defaults, but are this many readers needed?
+            mdb_result = mdb_env_set_maxreaders(mdb_env_ptr, 126);
+        }
+
+        if (mdb_result == 0)
+        {
+            // N.B. Using default flags for now, so flags value is 0x0
+            // TODO: Check the required state of MDB_NOTLS flag
+            unsigned int flags = 0x0;
+            mdb_result = mdb_env_open(mdb_env_ptr, dir_str.c_str(),
+                                      flags, 0755);
+        }
+    }
+
+    if (mdb_result != 0)
+    {
+        // Failure - call mdb_env_close() to discard the MDB_env handle
+        mdb_env_close(mdb_env_ptr);
+        mdb_env_ptr = nullptr;
+    }
+
     if (mdb_env_ptr != nullptr)
     {
         return true;
@@ -215,74 +270,6 @@ ProtectionDB::~ProtectionDB()
     
 }
 
-// -----------------------------------------------------------------------------
-#else // NOT PROTECTIONDB_RAII_VERSION
-// -----------------------------------------------------------------------------
-
-int ProtectionDB::doStuff()
-{
-    int major = 1;
-    int minor = 2;
-    int patch = 3;
-    char *lmdb_version_str = mdb_version(&major, &minor, &patch);
-
-    return 456;
-}
-
-bool ProtectionDB::getRawField(MDB_txn &txn, MDB_dbi dbi,
-                               const char serial_number[],
-                               const char subkey[], MDB_val &data_val_ret)
-{
-    std::string key_str = std::string(serial_number) + keystr_separator_char +
-                          subkey;
-    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
-
-    int mdb_result = mdb_get(&txn, dbi, &key_val, &data_val_ret);
-    
-    if (mdb_result == 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-void ProtectionDB::putField(MDB_txn &txn, MDB_dbi dbi, const char serial_number[],
-                            const char subkey[], MDB_val &data_val)
-{
-    // Create ASCII key of form <serial_number><separator><subkey>
-    // IMPORTANT: serial_number and subkey must not contain the
-    // keystr_separator_char character
-    std::string key_str = std::string(serial_number) + keystr_separator_char +
-                          subkey;
-    
-    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
-
-    // TODO: Check if final flags argument below is OK
-    int mdb_result = mdb_put(&txn, dbi, &key_val, &data_val, 0x0);
-
-    // TODO: Call mdb_txn_commit() here?? Check against the Python version
-    // AND where to call mdb_env_sync() to flush to disk?
-    
-    // TODO: Return a result value
-}
-
-void ProtectionDB::putCounters(MDB_txn &txn, MDB_dbi dbi, 
-                               const char serial_number[],
-                               const FpuCounters &fpu_counters)
-{
-    MDB_val data_val;
-    
-    data_val.mv_data = fpu_counters.getRawData(data_val.mv_size); 
-
-    putField(txn, dbi, serial_number, counters_keystr, data_val);
-
-    // TODO: Return a result value
-}
-
-
-// -----------------------------------------------------------------------------
-#endif // NOT PROTECTIONDB_RAII_VERSION
-// -----------------------------------------------------------------------------
 
 // N.B. C++ replacement for Python open_database_env()
 // TODO: Remove comment above once complete
@@ -295,7 +282,8 @@ MDB_env *protectionDB_OpenEnv(bool mockup)
     std::string dir_str = protectionDB_GetDirFromLinuxEnv(mockup);
     if (!dir_str.empty())
     {
-        env_ptr = protectionDB_OpenEnv(dir_str);
+        //************** TODO: Change to use ProtectionDB::open() instead
+        // env_ptr = protectionDB_OpenEnv(dir_str);
     }
         
     return env_ptr;
@@ -342,81 +330,7 @@ std::string protectionDB_GetDirFromLinuxEnv(bool mockup)
     
     return dir_str_ret;
 }
-
-// -----------------------------------------------------------------------------
     
-MDB_env *protectionDB_OpenEnv(const std::string &dir_str)
-{
-    // TODO: Must only call exactly once for a particular LMDB file in this
-    // process (see the LMDB documentation) - enforce this somehow?
-    
-    MDB_env *env_ptr = nullptr;
-    size_t dbsize;
-
-    // Needs 64 bit (large file support) for normal database size
-    // TODO: In original Python code, 32/64-bit OS check was done - but is
-    // this possible from C++? Or can we assume it's always 64-bit?
-    // (can make it a requirement?)
-    // if (platform.architecture()[0] == "64bit")  // Original Python
-    if (1)
-    {
-        dbsize = 5 * 1024L * 1024L * 1024L;
-    }
-    else
-    {
-        dbsize = 5 * 1024L * 1024L;
-    }
-
-    //.......................
-    // TODO: Python to c++ conversion WIP
-    // See https://lmdb.readthedocs.io/en/release/ -> Environment class
-    // env = lmdb.open(database_file_name, max_dbs=10, map_size=dbsize)
-
-    // TODO: In the original Python code, the following are defaulted - see
-    // https://lmdb.readthedocs.io/en/release/ -> Environment class - 
-    // max_spare_txns=1
-    // create=True      <== **IMPORTANT** - create directory if doesn't exist?
-
-    int mdb_result = mdb_env_create(&env_ptr);
-    if (mdb_result == 0)
-    {
-        mdb_result = mdb_env_set_maxdbs(env_ptr, 10);
-        if (mdb_result == 0)
-        {
-            mdb_result = mdb_env_set_mapsize(env_ptr, dbsize);
-        }
-
-        if (mdb_result == 0)
-        {
-            // TODO: The following keeps compatibility with the original
-            // Python code defaults, but are this many readers needed?
-            mdb_result = mdb_env_set_maxreaders(env_ptr, 126);
-        }
-
-        if (mdb_result == 0)
-        {
-            // N.B. Using default flags for now, so flags value is 0x0
-            // TODO: Check the required state of MDB_NOTLS flag
-            unsigned int flags = 0x0;
-            mdb_result = mdb_env_open(env_ptr, dir_str.c_str(),
-                                      flags, 0755);
-        }
-
-        if (mdb_result != 0)
-        {
-            // Failure - call mdb_env_close() to discard the MDB_env handle
-            mdb_env_close(env_ptr);
-            env_ptr = nullptr;
-        }
-    }
-
-    //.......................
-
-    // TODO: Return mdb_result somehow as well?
-
-    return env_ptr;
-}
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -554,7 +468,7 @@ static bool protectionDB_TestMultipleItemReadWrites(ProtectionDB &protectiondb)
                 uint64_t test_val = ((uint64_t)i) * test_multiplier;
                 void *data_returned_ptr = nullptr;
                 size_t num_bytes_returned = 0;
-                
+
                 char subkey_str[10];
                 snprintf(subkey_str, sizeof(subkey_str), "%03d", i);
                 if (fpudb_txn->test_ReadRawItem(serial_number_str.c_str(),
@@ -689,8 +603,141 @@ bool protectionDB_Test()
 
 
 // -----------------------------------------------------------------------------
+// TODO: Old experimentation code - delete once happy with new code above
 #ifndef PROTECTIONDB_RAII_VERSION // NOT PROTECTIONDB_RAII_VERSION
 // -----------------------------------------------------------------------------
+
+int ProtectionDB::doStuff()
+{
+    int major = 1;
+    int minor = 2;
+    int patch = 3;
+    char *lmdb_version_str = mdb_version(&major, &minor, &patch);
+
+    return 456;
+}
+
+bool ProtectionDB::getRawField(MDB_txn &txn, MDB_dbi dbi,
+                               const char serial_number[],
+                               const char subkey[], MDB_val &data_val_ret)
+{
+    std::string key_str = std::string(serial_number) + keystr_separator_char +
+                          subkey;
+    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
+
+    int mdb_result = mdb_get(&txn, dbi, &key_val, &data_val_ret);
+    
+    if (mdb_result == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void ProtectionDB::putField(MDB_txn &txn, MDB_dbi dbi, const char serial_number[],
+                            const char subkey[], MDB_val &data_val)
+{
+    // Create ASCII key of form <serial_number><separator><subkey>
+    // IMPORTANT: serial_number and subkey must not contain the
+    // keystr_separator_char character
+    std::string key_str = std::string(serial_number) + keystr_separator_char +
+                          subkey;
+    
+    MDB_val key_val = { key_str.size(), (void *)key_str.c_str() };
+
+    // TODO: Check if final flags argument below is OK
+    int mdb_result = mdb_put(&txn, dbi, &key_val, &data_val, 0x0);
+
+    // TODO: Call mdb_txn_commit() here?? Check against the Python version
+    // AND where to call mdb_env_sync() to flush to disk?
+    
+    // TODO: Return a result value
+}
+
+void ProtectionDB::putCounters(MDB_txn &txn, MDB_dbi dbi, 
+                               const char serial_number[],
+                               const FpuCounters &fpu_counters)
+{
+    MDB_val data_val;
+    
+    data_val.mv_data = fpu_counters.getRawData(data_val.mv_size); 
+
+    putField(txn, dbi, serial_number, counters_keystr, data_val);
+
+    // TODO: Return a result value
+}
+
+MDB_env *protectionDB_OpenEnv(const std::string &dir_str)
+{
+    // TODO: Must only call exactly once for a particular LMDB file in this
+    // process (see the LMDB documentation) - enforce this somehow?
+    
+    MDB_env *env_ptr = nullptr;
+    size_t dbsize;
+
+    // Needs 64 bit (large file support) for normal database size
+    // TODO: In original Python code, 32/64-bit OS check was done - but is
+    // this possible from C++? Or can we assume it's always 64-bit?
+    // (can make it a requirement?)
+    // if (platform.architecture()[0] == "64bit")  // Original Python
+    if (1)
+    {
+        dbsize = 5 * 1024L * 1024L * 1024L;
+    }
+    else
+    {
+        dbsize = 5 * 1024L * 1024L;
+    }
+
+    //.......................
+    // TODO: Python to c++ conversion WIP
+    // See https://lmdb.readthedocs.io/en/release/ -> Environment class
+    // env = lmdb.open(database_file_name, max_dbs=10, map_size=dbsize)
+
+    // TODO: In the original Python code, the following are defaulted - see
+    // https://lmdb.readthedocs.io/en/release/ -> Environment class - 
+    // max_spare_txns=1
+    // create=True      <== **IMPORTANT** - create directory if doesn't exist?
+
+    int mdb_result = mdb_env_create(&env_ptr);
+    if (mdb_result == 0)
+    {
+        mdb_result = mdb_env_set_maxdbs(env_ptr, 10);
+        if (mdb_result == 0)
+        {
+            mdb_result = mdb_env_set_mapsize(env_ptr, dbsize);
+        }
+
+        if (mdb_result == 0)
+        {
+            // TODO: The following keeps compatibility with the original
+            // Python code defaults, but are this many readers needed?
+            mdb_result = mdb_env_set_maxreaders(env_ptr, 126);
+        }
+
+        if (mdb_result == 0)
+        {
+            // N.B. Using default flags for now, so flags value is 0x0
+            // TODO: Check the required state of MDB_NOTLS flag
+            unsigned int flags = 0x0;
+            mdb_result = mdb_env_open(env_ptr, dir_str.c_str(),
+                                      flags, 0755);
+        }
+
+        if (mdb_result != 0)
+        {
+            // Failure - call mdb_env_close() to discard the MDB_env handle
+            mdb_env_close(env_ptr);
+            env_ptr = nullptr;
+        }
+    }
+
+    //.......................
+
+    // TODO: Return mdb_result somehow as well?
+
+    return env_ptr;
+}
 
 void protectionDB_Test()
 {
