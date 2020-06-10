4bf3daa97d81a44dd37073b13d7c37a7eb43751e
bwillemse <bwemail>
2020-06-10 11:30:24 +0100

    Grid driver: Added basic invocation to main() to test the build

--- NetBeansProjects/Fpu_NetBeans/main.cpp
+++ NetBeansProjects/Fpu_NetBeans/main.cpp
@@ -13,29 +13,56 @@
 
 #include <cstdlib>
 #include "ProtectionDB.h"
+#include "FPUGridDriver.h"
 
 using namespace std;
 
-/*
- * 
- */
+static void testFpuGridDriver();
+static void testFpuCounters();
+static void testProtectionDB();
+
+
+//------------------------------------------------------------------------------
 int main(int argc, char** argv)
 {
-    //..........................................................................
-    // Test FpuCounters class
-    FpuCounters fpu_counters;
+    testFpuGridDriver();
     
-    size_t num_bytes;
-    void *raw_data_ptr = fpu_counters.getRawData(num_bytes);
+    //testFpuCounters();
     
-    //..........................................................................
-    // Test ProtectionDB class
-    
-    bool result_ok = protectionDB_Test();
-   
-    
-    //..........................................................................
+    //testProtectionDB();
     
     return 0;
 }
 
+//------------------------------------------------------------------------------
+static void testFpuGridDriver()
+{
+    UnprotectedGridDriver unprotected_grid_driver;
+
+    // TODO: NOTE: I don't yet know what format of IP address is expected - just
+    // using a dummy format for now
+    // TODO: Also, t_gateway_address::ip is only a pointer - dangerous? Change
+    // this eventually? (e.g. to a std::String?)
+    const char *dummy_ip_str = "192.168.12.34";
+    t_gateway_address gateway_address = { dummy_ip_str, 12345 };
+
+    unprotected_grid_driver.connect(1, &gateway_address);
+    
+}
+
+//------------------------------------------------------------------------------
+static void testFpuCounters()
+{
+    FpuCounters fpu_counters;
+    size_t num_bytes;
+    
+    void *raw_data_ptr = fpu_counters.getRawData(num_bytes);
+}
+
+//------------------------------------------------------------------------------
+static void testProtectionDB()
+{
+    bool result_ok = protectionDB_Test();
+}
+
+//------------------------------------------------------------------------------
