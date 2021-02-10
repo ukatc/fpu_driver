0e7a9b2fe8af5b62f47572ae9841818d4b6b91b4
bwillemse <bwemail>
2021-02-10 11:36:07 +0000

    Error code improvements: Added ErrorCodes.C/h skeleton files

---
+++ include/ErrorCodes.h
@@ -0,0 +1,32 @@
+// -*- mode: c++ -*-
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2021 UKRI. See file "LICENSE" for license information.
+//
+//
+// Who       When        What
+// --------  ----------  -------------------------------------------------------
+// bwillemse 2021-02-10  Created.
+//------------------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////////////
+// NAME ErrorCodes.h
+//
+// Specifies all grid driver error codes and their groupings.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ERRORCODES_H
+#define ERRORCODES_H
+
+namespace mpifps
+{
+
+
+
+
+
+} // namespace mpifps
+
+#endif // ERRORCODES_H
