7d14191f62205f035fd55ccb4de124f7ee1cb4d7
bwillemse <bwemail>
2021-04-08 11:52:09 +0100

    Flexible CAN mapping: Enabled the FLEXIBLE_CAN_MAPPING macro

--- include/InterfaceConstants.h
+++ include/InterfaceConstants.h
@@ -36,7 +36,7 @@
 // in all projects, because the projects ideally all need to have the flexible
 // CAN mapping enabled or disabled together.
 
-// #define FLEXIBLE_CAN_MAPPING
+#define FLEXIBLE_CAN_MAPPING
 
 //**********************************
 //**********************************
